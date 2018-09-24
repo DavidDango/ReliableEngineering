00b6d9911018705a1998dde5e26cf3b27d0edbfa
DavidDango <david.ra.157@gmail.com>
2018-09-24 15:59:58 -0300

    First try.

--- finder.c
+++ finder.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+
+
+int main(int argc, char** args) {
     int n;
+    FILE *fptr;
+    int blocksize = 4096;
+    char* buf;
     
+    if((fptr = fopen(args[1], "rb")) == NULL){
+        printf("Error opening file \n");
+        exit(1);
+    }
+    
+    fseek(fptr, -5*sizeof(char), SEEK_END);
+    
+    for(n = 1; n < 5; ++n) {
+        fread(buf, sizeof(char), 5, fptr); 
+        printf("read: %s\n", buf);
+        fseek(fptr, -10*(sizeof(char)), SEEK_CUR);
+   }
+   fclose(fptr); 
 }
